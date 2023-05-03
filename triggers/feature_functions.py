ce631256ed74bd4cf19dac6118a6344bfd02f4b3
charantej224 <charantej.career@gmail.com>
2023-05-02 22:27:50 -0500

    feature functions

---
+++ triggers/feature_functions.py
@@ -0,0 +1,18 @@
+def function_one():
+    print("function one")
+
+
+def function_two():
+    print("function one")
+
+
+def function_three():
+    print("function one")
+
+
+def function_four():
+    print("function one")
+
+
+def function_five():
+    print("function one")
